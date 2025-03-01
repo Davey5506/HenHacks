b5dc023226baf964554d2ab92babd75ad9ce039a
Davey5506 <kpdmmvi@gmail.com>
2025-03-01 14:18:29 -0500

    Added comments to each code block.

--- backend/gemini_interface.py
+++ backend/gemini_interface.py
@@ -15,17 +15,22 @@ async def interprit_image(client:Client, image:Image):
     return response.text
 
 async def main(path:str) -> str:
+    # Load the API key from the .env file
     api_key = dotenv.dotenv_values(".env").get("API_KEY")
     if api_key is None:
         print("API_KEY not set")
         exit(-1)
 
+    # Connect to the Gemini API
     client = genai.Client(api_key=api_key)
     if client is None:
         print("Could not connect to gemini")
         exit(-2)
 
+    # Load the image and pre-process it
     image = Image.open(path)
     image = await preprocessor.resize_image(image)
+
+    # Send the image to the Gemini API
     response = await interprit_image(client, image)
     return response
--- backend/preprocessor.py
+++ backend/preprocessor.py
@@ -1,14 +1,17 @@
 from PIL import Image
 
 async def resize_image(image: Image) -> Image:
+    # Get the current size of the image
     width, height = image.size
 
+    # Check if current size is larger than 480p resolution
     if height > 480:
         new_height = 480
         new_width = int(width * (new_height/float(height)))
     else:
         new_width, new_height = width, height
-    
+
+    # Resize the image
     resized = image.resize((new_width, new_height))
     return resized
 
