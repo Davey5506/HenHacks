7fb8afd51722240235c1f5312ba1e41c1eb9126d
NickWang25 <nw201607@gmail.com>
2025-03-01 14:36:03 -0500

    Created functions to adjust contrast, brightness, and saturation

--- backend/preprocessor.py
+++ backend/preprocessor.py
@@ -1,4 +1,5 @@
-from PIL import Image
+from PIL import Image, ImageEnhance
+
 
 async def resize_image(image: Image) -> Image:
     width, height = image.size
@@ -13,10 +14,20 @@ async def resize_image(image: Image) -> Image:
     return resized
 
 async def adjust_contrast(image: Image) -> Image:
+    try:
+        enhancer = ImageEnhance.Contrast(image)
+        image = enhancer.enhance(1.5)
+    except Exception as e:
+        print(e)
+
     return image
 
 async def adjust_brightness(image: Image) -> Image:
+    enhancer = ImageEnhance.Brightness(image)
+    image = enhancer.enhance(1.5)
     return image
 
 async def adjust_saturation(image: Image) -> Image:
+    enhancer = ImageEnhance.Color(image)
+    image = enhancer.enhance(1.5)
     return image
