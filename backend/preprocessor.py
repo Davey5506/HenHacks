290f3d7f8ba3205dd08f8fa96b1e0d7ab2b6b900
NickWang25 <nw201607@gmail.com>
2025-03-01 13:27:19 -0500

    Created resize_img function

--- backend/preprocessor.py
+++ backend/preprocessor.py
@@ -1,3 +1,21 @@
 import cv2
 import asyncio
+from PIL import Image
+
+async def resize_image(input, output):
+    image = Image.open(input)
+    width, height = image.size()
+
+    newWidth = 0
+    newHeight = 0
+    if height > 480:
+        newHeight = 480
+        newWidth = int(width * (newHeight/float(height)))
+    else:
+        newWidth, newHeight = width, height
+    
+    resized = image.resize((newWidth, newHeight), Image.LANCZOS)
+    resized.save(output)
+
+
 
